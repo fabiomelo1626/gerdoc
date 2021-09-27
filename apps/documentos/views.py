df3a8d7a28cefc70875ab363ca0870d7d9ea052d
fabiomelo1626 <fabiomelo.1626@gmail.com>
2021-09-27 15:52:04 -0300

    add view-documentos

--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 from .forms import DocumentosForm
 from .models import *
 
 
+class NovoDocumento(CreateView):
+    form_class = DocumentosForm
+    
