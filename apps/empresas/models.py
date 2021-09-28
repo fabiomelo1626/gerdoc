a36fe165928b5f6f8c2d66ccce24db677ebd8f49
Arthur <fabio+arthur@gerdoc.com>
2021-09-28 11:37:42 -0300

    Tela de cadastro de empresa finalizada

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Empresas(models.Model):
     razao_social = models.CharField('Razao Social', max_length=100, unique=True)
     nome_fantasia = models.CharField('Nome Fantasia', max_length=100)
-    cnpj = models.CharField('CNPJ', max_length=15, unique=True)
+    cnpj = models.CharField('CNPJ', max_length=18, unique=True)
     endereco = models.CharField('Endereco', max_length=200)
     email = models.EmailField('Email')
-    telefone = models.CharField('Telefone', max_length=14)
+    telefone = models.CharField('Telefone', max_length=15)
 
     def __str__(self):
         return self.razao_social
