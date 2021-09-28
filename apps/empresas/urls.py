dddec188d753754348bb23bd9fc21fed7f9d46f5
Arthur <fabio+arthur@gerdoc.com>
2021-09-28 14:49:45 -0300

    Tel de informações da empresa pronta e view de deletar empresa

--- apps/empresas/urls.py
+++ apps/empresas/urls.py
@@ -8,4 +8,6 @@ app_name = 'empresas'
 urlpatterns = [
     path('nova-empresa/', login_required(NovaEmpresa.as_view()), name='nova_empresa'),
     path('lista-empresas/', login_required(ListaEmpresas.as_view()), name='lista_empresas'),
+    path('detalhe-empresa/<int:pk>/', login_required(DetalheEmpresa.as_view()), name='detalhe_empresa'),
+    path('deletar-empresa/<int:pk>/', login_required(DeletarEmpresa.as_view()), name='deletar_empresa'),
 ]
